aed06a205c8091abdf66c15ee800a14df888e593
Aksh2026 <akshayatestleaf@gmail.com>
2026-03-01 20:32:53 +0530

    Add files via upload
    Day2 week1
    Find odd numbers

---
+++ FindOddNumber.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class FindOddNumber{
+           
+public static void main(String[] args) {
+    System.out.println("the list of odd numbers are");
+    for(int i=0;i<10;i++){
+        if(i%2!=0){
+            System.out.println(i);
+        }
+    }
+}
+}
